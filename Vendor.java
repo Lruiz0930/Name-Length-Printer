65b55158bfed99c43204cf61443301db7c92d500
Lruiz0930 <30706041+Lruiz0930@users.noreply.github.com>
2019-04-03 08:30:45 -0700

    Add files via upload

---
+++ Vendor.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Vendor {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        boolean foundIt = false;
+        int[] thisArray = new int[3];
+        
+        for (int i = 0; i < thisArray.length; i++){
+            System.out.println("Please enter the vendors number: ");
+            thisArray[i] = input.nextInt();
+        }
+            System.out.println("What number are you looking for: ");
+            int num = input.nextInt();
+            
+        for (int i = 0; i < thisArray.length; i++){
+            if (thisArray[i] == num)
+            foundIt = true;
+        }
+        if(foundIt == true)
+            System.out.println("We found it!");
+        else 
+            System.out.println("That wasn't there.");
+    }
+}
